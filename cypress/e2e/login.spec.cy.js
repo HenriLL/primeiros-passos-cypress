d7dac48f1cc0e5550e7aa6f5e67728123f70c53b
unknown <hnamix@gmail.com>
2025-02-11 02:21:26 -0300

    Added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,19 +1,28 @@
+const elementList ={
+  visitSite: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',
+  usernameField: "[name='username']",
+  passwordField: "[name='password']",
+  loginButton: "[type='submit']",
+  sectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
+  wrongCredentialAlert: "[role='alert']"
+}
+
 describe('Orange HRM Tests', () => {
   it('Login sucess', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
-    cy.get('.oxd-button').click()
+    cy.visit(elementList.visitSite)
+    cy.get(elementList.usernameField).type('Admin')
+    cy.get(elementList.passwordField).type('admin123')
+    cy.get(elementList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+    cy.get(elementList.sectionTitleTopBar).contains('Dashboard')
   }) 
 
   it('Login Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin12')
-    cy.get('.oxd-button').click()
-    cy.get('.oxd-alert')
+    cy.visit(elementList.visitSite)
+    cy.get(elementList.usernameField).type('Test')
+    cy.get(elementList.passwordField).type('test')
+    cy.get(elementList.loginButton).click()
+    cy.get(elementList.wrongCredentialAlert)
     
   })
 })

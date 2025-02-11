0ad6fd0a801412c91ead992f394cd72751517ef5
unknown <hnamix@gmail.com>
2025-02-11 02:55:06 -0300

    created userData

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -4,14 +4,26 @@ const elementList ={
   passwordField: "[name='password']",
   loginButton: "[type='submit']",
   sectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
+  dashboardGrid: "orangehrm-dashboard-grid",
   wrongCredentialAlert: "[role='alert']"
+}
+
+const userData ={
+  userSucess:{
+    username: 'Admin',
+    password: 'admin123'
+  },
+  userFail:{
+    username: 'teste',
+    password: 'teste'
+  }
 }
 
 describe('Orange HRM Tests', () => {
   it('Login sucess', () => {
     cy.visit(elementList.visitSite)
-    cy.get(elementList.usernameField).type('Admin')
-    cy.get(elementList.passwordField).type('admin123')
+    cy.get(elementList.usernameField).type(userData.userSucess.username)
+    cy.get(elementList.passwordField).type(userData.userSucess.password)
     cy.get(elementList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(elementList.sectionTitleTopBar).contains('Dashboard')
@@ -19,8 +31,8 @@ describe('Orange HRM Tests', () => {
 
   it('Login Fail', () => {
     cy.visit(elementList.visitSite)
-    cy.get(elementList.usernameField).type('Test')
-    cy.get(elementList.passwordField).type('test')
+    cy.get(elementList.usernameField).type(userData.userFail.username)
+    cy.get(elementList.passwordField).type(userData.userFail.password)
     cy.get(elementList.loginButton).click()
     cy.get(elementList.wrongCredentialAlert)
     

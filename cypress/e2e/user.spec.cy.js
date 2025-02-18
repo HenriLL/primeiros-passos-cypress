c670aba299e9401e49561a73e83867227804afc1
unknown <hnamix@gmail.com>
2025-02-18 12:55:15 -0400

    Modifing name, midle and last name from user

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -8,18 +8,26 @@ describe('template spec', () => {
     passwordField: "[name='password']",
     loginButton: ".oxd-button",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    wrongCredentialAlert: ".oxd-alert"
+    wrongCredentialAlert: ".oxd-alert",
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: "[name='firstName']",
+    midleNameField: "[name='middleName']",
+    lastNameField: "[name='lastName']",
   }
 
   
   
-  it('Login - passes', () => {
+  it.only('User Info Update - Sucesses', () => {
     cy.visit('/auth/login')
     cy.get(selectorList.userNameField).type(userData.userSucess.username)
     cy.get(selectorList.passwordField).type(userData.userSucess.password)
     cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorList.myInfoButton).click()
+    cy.get(selectorList.firstNameField).clear().type('First Name')
+    cy.get(selectorList.midleNameField).clear().type('Midle Name')
+    cy.get(selectorList.lastNameField).clear().type('Last Name')
   })
 
   it('Login - Fail', () => {

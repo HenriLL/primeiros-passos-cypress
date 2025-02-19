6231c5e394159de9312074889d493f797e96dd8e
unknown <hnamix@gmail.com>
2025-02-19 01:20:28 -0300

    User change scenario

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -13,6 +13,12 @@ describe('template spec', () => {
     firstNameField: "[name='firstName']",
     midleNameField: "[name='middleName']",
     lastNameField: "[name='lastName']",
+    genericField: ".oxd-input",
+    dateField: "[placeholder='yyyy-dd-mm']",
+    buttonSave: "[type='submit']",
+    testField: "[options='']",
+    dateCloseButton: ".--close",
+    //dateBirthdayCloseButton: '.oxd-toast-container--bottom',
   }
 
   
@@ -25,9 +31,19 @@ describe('template spec', () => {
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
     cy.get(selectorList.myInfoButton).click()
-    cy.get(selectorList.firstNameField).clear().type('First Name')
-    cy.get(selectorList.midleNameField).clear().type('Midle Name')
-    cy.get(selectorList.lastNameField).clear().type('Last Name')
+    cy.get(selectorList.firstNameField).clear().type('First Name Test')
+    cy.get(selectorList.midleNameField).clear().type('Midle Name Test')
+    cy.get(selectorList.lastNameField).clear().type('Last Name Test')
+    cy.get(selectorList.genericField).eq(4).clear().type('EmploTest')
+    cy.get(selectorList.genericField).eq(5).clear().type(777)
+    cy.get(selectorList.genericField).eq(6).clear().type(12345)
+    cy.get(selectorList.dateField).eq(0).clear().type('2025-10-12')
+    cy.get(selectorList.dateCloseButton).eq(0).click()
+    cy.get(selectorList.dateField).eq(1).clear().type('1990-21-04')
+    //cy.get(selectorList.dateBirthdayCloseButton).click()
+    cy.get(selectorList.buttonSave).eq(0).click()
+    cy.get('.oxd-toast')
+    cy.get(selectorList.testField).clear().type('Test 777')
   })
 
   it('Login - Fail', () => {
